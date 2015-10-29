21d553e60ba1f3b4cad8bf9d3a869b1a2be2e11a
Gregg Lewis <code@gregglewis.net>
2015-10-28 21:55:33 -0600

    game generator

--- scorehistory.js
+++ scorehistory.js
@@ -3,7 +3,7 @@ var context = document.getElementById("scoreHistory").getContext("2d");
 
 var data = 
 {
-    labels: ["1","2","3","4","5","6","7","8","9"],
+    labels: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"],
     datasets:
     [
         {
