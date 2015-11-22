7f0d9448d4be813cfc52777ba2defe7e11c63536
Gregg Lewis <code@gregglewis.net>
2015-11-22 01:26:48 -0500

    add a shortID to player

--- django_leaderboard/scores/models.py
+++ django_leaderboard/scores/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Player(models.Model):
+    short_id = models.CharField(max_length=200, primary_key=True,default="CHUMP")
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     handle = models.CharField(max_length=200)
