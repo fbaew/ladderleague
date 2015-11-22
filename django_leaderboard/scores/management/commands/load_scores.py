f5b2b37db62ae4d0a08985fffadfd6ab4d7412e0
Gregg Lewis <code@gregglewis.net>
2015-11-22 03:10:27 -0500

    manage.py load_scores: now it prints

--- django_leaderboard/scores/management/commands/load_scores.py
+++ django_leaderboard/scores/management/commands/load_scores.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 #from django.utils import timezone
 from scores.models import Player, Game, Set
+import csv
 
 class Command(BaseCommand):
     args = ""
@@ -8,5 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         '''here we will load and parse the file...'''
-        print("Not yet implemented, dawg.")
-        pass
+
+        with open('/home/gregg/devel/scores/data/scores.csv','r') as csvfile:
+            reader = csv.reader(csvfile,delimiter=',')
+            for row in reader:
+                print(row)
