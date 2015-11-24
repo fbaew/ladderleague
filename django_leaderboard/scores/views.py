59196878cf109c6c697bace32adcf3beaf796340
Gregg Lewis <code@gregglewis.net>
2015-11-23 23:25:54 -0500

    rendering our page from a template... static files are broken atm

--- django_leaderboard/scores/views.py
+++ django_leaderboard/scores/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from scores.models import Player, Game, Set
+from django.template import RequestContext, loader
 
 # Create your views here.
 
 def leaders(request):
-    return HttpResponse('Hello')
+    player_list = Player.objects.all()
+    template = loader.get_template('scores/index.html')
+    context = RequestContext(request, {
+        'players':player_list,
+    })
+    return HttpResponse(template.render(context))
