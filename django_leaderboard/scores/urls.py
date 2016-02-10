3f760d79daa7c4a7eea1c9fb13d8643a36e34491
Gregg Lewis <code@gregglewis.net>
2016-02-10 01:47:38 -0500

    show the leaders in random order on the index page

--- django_leaderboard/scores/urls.py
+++ django_leaderboard/scores/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.leaders,name='leaders'),
+    url(r'^$', views.index, name='index'),
+    url(r'^leaders$', views.leaders, name='leaders'),
 ]
--- django_leaderboard/scores/views.py
+++ django_leaderboard/scores/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from scores.models import Player, Game, Set
 from django.template import RequestContext, loader
+import random
 
 # Create your views here.
 
@@ -14,9 +15,15 @@ from django.template import RequestContext, loader
 #     return HttpResponse(template.render(context))
 # 
 def leaders(request):
-     player_list = Player.objects.all()
-     template = loader.get_template('scores/index.html')
+    player_list = Player.objects.all()
+    template = loader.get_template('scores/leaders.html')
 
-     return HttpResponse(template.render({
-        'players':player_list,
-        },request))
+    return HttpResponse(template.render({
+       'leaders':player_list,
+       }, request))
+
+def index(request):
+    player_list = Player.objects.order_by("?") #performance; only temporary
+    template = loader.get_template('scores/index.html')
+    html = template.render({'leaders':player_list},request)
+    return HttpResponse(html)
