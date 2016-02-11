cbd8c0c653f16f6b1de90b5d4659638b911abef6
Gregg Lewis <code@gregglewis.net>
2016-02-10 23:15:31 -0500

    adding templates and stuff that I was too tired to remember last night

---
+++ django_leaderboard/scores/management/commands/recalc_stats.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+#from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+
+
+from scores.models import Player, Game, Set
+import csv
+
+class Command(BaseCommand):
+    args = ""
+    help = "Calculates basic statistics for all players"
+
+    def handle(self, *args, **options):
+        '''Clear everyone's regulation wins, and repopulate.'''
+        for player in Player.objects.all():
+            player.regulation_wins = 0
+            player.regulation_losses = 0
+        for contest in Set.objects.all():
+            winner = contest.winner()
+            loser = contest.opponent(winner)
+            print("winner: {}".format(winner))
+            winner.regulation_wins += 1
+            winner.save()
+            loser.regulation_losses += 1
+            loser.save()
+
+        for player in Player.objects.all():
+            player.elo_rating = \
+                (player.regulation_wins+1.0)/(player.regulation_losses+1.0)
+            player.save()
