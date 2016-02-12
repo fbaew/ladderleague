2032bdcd1fbeef3ae947268d4dc317b15ccc51b6
Gregg Lewis <code@gregglewis.net>
2016-02-12 01:41:04 -0500

    fixing recalc_stats to use exception for ties

--- django_leaderboard/scores/management/commands/recalc_stats.py
+++ django_leaderboard/scores/management/commands/recalc_stats.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 #from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
-
+from scores.exceptions import *
 
 from scores.models import Player, Game, Set
 import csv
@@ -15,14 +15,18 @@ class Command(BaseCommand):
         for player in Player.objects.all():
             player.regulation_wins = 0
             player.regulation_losses = 0
+            player.save()
         for contest in Set.objects.all():
-            winner = contest.winner()
-            loser = contest.opponent(winner)
-            print("winner: {}".format(winner))
-            winner.regulation_wins += 1
-            winner.save()
-            loser.regulation_losses += 1
-            loser.save()
+            try:
+                winner = contest.winner()
+                loser = contest.opponent(winner)
+                print("winner: {}".format(winner))
+                winner.regulation_wins += 1
+                winner.save()
+                loser.regulation_losses += 1
+                loser.save()
+            except UndefinedOutcomeError:
+                pass
 
         for player in Player.objects.all():
             player.elo_rating = \
