d40b0e0478e86659a3c6f24e8c844828a453ab30
Gregg Lewis <code@gregglewis.net>
2016-02-16 00:12:57 -0500

    really fixing recalc_stats

--- ladderleague_app/scores/management/commands/recalc_stats.py
+++ ladderleague_app/scores/management/commands/recalc_stats.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
 
         for player in Player.objects.all():
             player.ladder_rank = round(
-                (player.regulation_wins+1.0)/(player.regulation_losses+1.0),
+                (player.wins+1.0)/(player.wins+1.0),
                 2
             )
             player.save()
