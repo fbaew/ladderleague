d610165e637f0c37ff7243e67d89ad56ae252b42
Gregg Lewis <code@gregglewis.net>
2016-02-16 22:02:39 -0500

    trying to fix URL for static content still

---
+++ ladderleague_app/frontend/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(
+        r'^(?P<player_name>\w.*)/(?P<contest_number>\d.*)$',
+        views.contest_overview,
+        name="contest_overview"
+    ),
+    url(
+        r'^(?P<player_name>\w.*/?)$',
+        views.player_overview,
+        name='player_overview'
+    ),
+]
+
+
